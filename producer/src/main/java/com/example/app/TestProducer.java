e6c6c67fa9615b7ed9d07105cbd8dbbc40421722
Basil Hariri <basilhariri@gmail.com>
2018-09-27 19:38:54 +0000

    Test change

--- producer/src/main/java/com/example/app/TestProducer.java
+++ producer/src/main/java/com/example/app/TestProducer.java
@@ -17,14 +17,16 @@ import java.util.concurrent.Executors;
 public class TestProducer {
     private final static int NUM_THREADS = 5;
     private final static String CONFIG_PATH = "src/main/resources/producer.config";
+    private final static String CONNECTION_STRING = System.getenv("CONNECTION_STRING");
+    private final static String FQDN = CONNECTION_STRING.substring(CONNECTION_STRING.indexOf("sb://") + 5, CONNECTION_STRING.indexOf("/;")) + ":9093";
 
     public static void main(String... args) throws Exception {
         //Create Kafka Producer
-	final String CONNECTION_STRING = System.getenv("CONNECTION_STRING");
-	final String FQDN = CONNECTION_STRING.substring(CONNECTION_STRING.indexOf("sb://") + 5, CONNECTION_STRING.indexOf("/;")) + ":9093";
+//	final String CONNECTION_STRING = System.getenv("CONNECTION_STRING");
+//	final String FQDN = CONNECTION_STRING.substring(CONNECTION_STRING.indexOf("sb://") + 5, CONNECTION_STRING.indexOf("/;")) + ":9093";
 	//System.out.println("CONNECTION STRING = " + CONNECTION_STRING + "\nFQDN = " + FQDN);
 	//System.exit(1);
-        final Producer<Long, String> producer = createProducer();
+        final Producer<Long, String> producer = createProducer(CONNECTION_STRING);
         final ExecutorService executorService = Executors.newFixedThreadPool(NUM_THREADS);
                                                                                                                    
         //Run several CDRs                                                                                         
@@ -38,8 +40,10 @@ public class TestProducer {
     private static Producer<Long, String> createProducer() {                                                       
         try {                                                                                                      
             Properties properties = new Properties();                                                              
-            properties.load(new FileReader(CONFIG_PATH));                                                          
-            properties.put(ProducerConfig.CLIENT_ID_CONFIG, "java-spark-producer");                                
+            properties.load(new FileReader(CONFIG_PATH));
+	    properties.put("sasl.jaas.config", properties.get("sasl.jaas.config").replace("{YOUR.EVENTHUBS.CONNECTION.STRING}", CONNECTION_STRING));
+	    properties.put("bootstrap.servers", FQDN);
+	    properties.put(ProducerConfig.CLIENT_ID_CONFIG, "java-producer");                                
             properties.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, LongSerializer.class.getName());            
             properties.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());        
             for (Object s : properties.keySet())                                                                   
