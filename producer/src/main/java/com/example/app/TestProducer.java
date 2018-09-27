822b8e7397432d9e9ce1f1e601b2766aea345c6c
Basil Hariri <basilhariri@gmail.com>
2018-09-27 23:06:28 +0000

    Whitespace updates

--- producer/src/main/java/com/example/app/TestProducer.java
+++ producer/src/main/java/com/example/app/TestProducer.java
@@ -14,18 +14,17 @@ import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutorService;                                                                       
 import java.util.concurrent.Executors;                                                                             
                                                                                                                    
-public class TestProducer {
+public class TestProducer 
+{
     private final static int NUM_THREADS = 5;
     private final static String CONFIG_PATH = "src/main/resources/producer.config";
     private final static String CONNECTION_STRING = System.getenv("CONNECTION_STRING");
     private final static String FQDN = CONNECTION_STRING.substring(CONNECTION_STRING.indexOf("sb://") + 5, CONNECTION_STRING.indexOf("/;")) + ":9093";
+    private final static String PRODUCER_ID = System.getenv("PRODUCER_ID");
 
-    public static void main(String... args) throws Exception {
+    public static void main(String... args) throws Exception 
+    {
         //Create Kafka Producer
-//	final String CONNECTION_STRING = System.getenv("CONNECTION_STRING");
-//	final String FQDN = CONNECTION_STRING.substring(CONNECTION_STRING.indexOf("sb://") + 5, CONNECTION_STRING.indexOf("/;")) + ":9093";
-	//System.out.println("CONNECTION STRING = " + CONNECTION_STRING + "\nFQDN = " + FQDN);
-	//System.exit(1);
         final Producer<Long, String> producer = createProducer();
         final ExecutorService executorService = Executors.newFixedThreadPool(NUM_THREADS);
                                                                                                                    
@@ -37,13 +36,15 @@ public class TestProducer {
     private static final Logger logger =                                                                           
             LoggerFactory.getLogger(DataReporter.class);                                                           
                                                                                                                    
-    private static Producer<Long, String> createProducer() {                                                       
-        try {                                                                                                      
+    private static Producer<Long, String> createProducer() 
+    {                                                       
+        try 
+	{                                                                                                      
             Properties properties = new Properties();                                                              
             properties.load(new FileReader(CONFIG_PATH));
 	    properties.put("sasl.jaas.config", properties.getProperty("sasl.jaas.config").replace("{YOUR.EVENTHUBS.CONNECTION.STRING}", CONNECTION_STRING));
 	    properties.put("bootstrap.servers", FQDN);
-	    properties.put(ProducerConfig.CLIENT_ID_CONFIG, "java-producer");                                
+	    properties.put(ProducerConfig.CLIENT_ID_CONFIG, "java-producer-" + PRODUCER_ID);
             properties.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, LongSerializer.class.getName());            
             properties.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());        
             for (Object s : properties.keySet())                                                                   
@@ -51,7 +52,9 @@ public class TestProducer {
                 System.out.println(s + ":" + properties.get(s));                                                   
             }                                                                                                      
             return new KafkaProducer<>(properties);                                                                
-        } catch (Exception e){                                                                                     
+        } 
+	catch (Exception e)
+	{                                                                                     
             System.out.println("Exception: " + e);                                                                 
             System.exit(1);                                                                                        
             return null;                                                                                           
