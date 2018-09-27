527ee78ad27ac880c89ce9d72408bc1d06c6dc39
Basil Hariri <8660137+basilhariri@users.noreply.github.com>
2018-09-27 11:58:33 -0700

    Update TestProducer.java

--- producer/src/main/java/com/example/app/TestProducer.java
+++ producer/src/main/java/com/example/app/TestProducer.java
@@ -1,48 +1,52 @@
 //Copyright (c) Microsoft Corporation. All rights reserved.
 //Licensed under the MIT License.
-import org.apache.kafka.clients.producer.KafkaProducer;
-import org.apache.kafka.clients.producer.Producer;
-import org.apache.kafka.clients.producer.ProducerConfig;
-import org.apache.kafka.common.serialization.LongSerializer;
-import org.apache.kafka.common.serialization.StringSerializer;
-import java.util.Properties;
-import java.io.FileReader;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-
-
-public class TestProducer {
-    //Change constant to send messages to the desired topic, for this example we use 'test'
-    private final static String TOPIC = "test";
-        
-    private final static int NUM_THREADS = 1;
-
-
-    public static void main(String... args) throws Exception {
-        //Create Kafka Producer
-        final Producer<Long, String> producer = createProducer();
-
-        final ExecutorService executorService = Executors.newFixedThreadPool(NUM_THREADS);
-
-        //Run NUM_THREADS TestDataReporters
-        for (int i = 0; i < NUM_THREADS; i++)
-            executorService.execute(new TestDataReporter(producer, TOPIC));
-    }
-
-    private static Producer<Long, String> createProducer() {
-        try{
-            Properties properties = new Properties();
-            properties.load(new FileReader("src/main/resources/producer.config"));
-            properties.put(ProducerConfig.CLIENT_ID_CONFIG, "KafkaExampleProducer");
-            properties.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, LongSerializer.class.getName());
-            properties.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
-            return new KafkaProducer<>(properties);
-        } catch (Exception e){
-            System.out.println("Failed to create producer with exception: " + e);
-            System.exit(0);
-            return null;        //unreachable
-        }
-    }
+import org.apache.kafka.clients.producer.KafkaProducer;                                                            
+import org.apache.kafka.clients.producer.Producer;                                                                 
+import org.apache.kafka.clients.producer.ProducerConfig;                                                           
+import org.apache.kafka.clients.producer.ProducerRecord;                                                           
+import org.apache.kafka.common.serialization.LongSerializer;                                                       
+import org.apache.kafka.common.serialization.StringSerializer;                                                     
+import org.slf4j.Logger;                                                                                           
+import org.slf4j.LoggerFactory;                                                                                                                                                                                      
+import java.io.FileReader;                                                                                         
+import java.util.Properties;                                                                                       
+import java.util.concurrent.CompletableFuture;                                                                     
+import java.util.concurrent.ExecutorService;                                                                       
+import java.util.concurrent.Executors;                                                                             
+                                                                                                                   
+public class TestProducer {                                                                                                                                                                                       
+    private final static int NUM_THREADS = 20;                                                                     
+    private final static String CONFIG_PATH = "src/main/resources/producer.config";                                
+                                                                                                                                                                                                             
+    public static void main(String... args) throws Exception {                                                     
+        //Create Kafka Producer                                                                                    
+        final Producer<Long, String> producer = createProducer();                                                  
+        final ExecutorService executorService = Executors.newFixedThreadPool(NUM_THREADS);                         
+                                                                                                                   
+        //Run several CDRs                                                                                         
+        for (int i = 0; i < NUM_THREADS; i++)                                                                      
+            executorService.execute(new DataReporter(producer, CONFIG_PATH));                                      
+    }                                                                                                              
+                                                                                                                   
+    private static final Logger logger =                                                                           
+            LoggerFactory.getLogger(DataReporter.class);                                                           
+                                                                                                                   
+    private static Producer<Long, String> createProducer() {                                                       
+        try {                                                                                                      
+            Properties properties = new Properties();                                                              
+            properties.load(new FileReader(CONFIG_PATH));                                                          
+            properties.put(ProducerConfig.CLIENT_ID_CONFIG, "java-spark-producer");                                
+            properties.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, LongSerializer.class.getName());            
+            properties.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());        
+            for (Object s : properties.keySet())                                                                   
+            {                                                                                                      
+                System.out.println(s + ":" + properties.get(s));                                                   
+            }                                                                                                      
+            return new KafkaProducer<>(properties);                                                                
+        } catch (Exception e){                                                                                     
+            System.out.println("Exception: " + e);                                                                 
+            System.exit(1);                                                                                        
+            return null;                                                                                           
+        }                                                                                                          
+    }                                                                                                              
 }
-
-
