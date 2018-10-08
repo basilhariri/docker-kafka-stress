944b934fdd3e4075769e14eb3ae02b91214238e2
Basil Hariri <basilhariri@gmail.com>
2018-10-08 15:57:56 -0700

    Adding normal kafka consumer

---
+++ kafka-producer/src/main/java/com/example/app/DataReporter.java
@@ -0,0 +1,67 @@
+import org.apache.kafka.clients.producer.Producer;
+import org.apache.kafka.clients.producer.ProducerRecord;
+
+import java.io.FileReader;
+import java.sql.Timestamp;
+import java.util.Properties;
+import java.util.concurrent.ExecutionException;
+import java.util.Date;
+
+public class DataReporter implements Runnable 
+{
+    //Constants
+    private String CONFIG_PATH;
+    //Instance
+    private Producer<Long, String> producer;
+
+    public DataReporter(final Producer<Long, String> producer)
+    {
+        this.producer = producer;
+    }
+
+    public String createRandomString(int sentCount, int length)
+    {
+        try
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.append(sentCount + " ");
+            while(sb.length() < length)
+            {
+                sb.append(" ");
+            }
+            return sb.toString();
+        }
+        catch (Exception e)
+        {
+            System.out.println(e);
+            return null;
+        }
+    }
+
+    @Override
+    public void run() 
+    {
+        int sentCount = 0;
+        while (true) 
+        {
+            try 
+            {
+                long time = System.currentTimeMillis();
+                String s = createRandomString(sentCount, 1000);
+                System.out.println(new Date(time) + " " + s.substring(0, 30));
+                final ProducerRecord<Long, String> record = new ProducerRecord<Long, String>(System.getenv("TOPIC"), time, s);
+                producer.send(record).get();
+                sentCount++;
+            } 
+            catch (InterruptedException e) 
+            {
+                Thread.interrupted();
+                break;
+            } 
+            catch (Exception e)
+            {
+                System.out.println(e);
+            }
+        }
+    }
+}
