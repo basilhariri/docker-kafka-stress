b3a9331e0408078957ef478994b41ee44a45f650
Basil Hariri <basilhariri@gmail.com>
2018-11-14 12:54:20 -0800

    Adding car producer

---
+++ car/eventhubs-producer/src/main/java/CarDataReporter.java
@@ -0,0 +1,146 @@
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.microsoft.applicationinsights.extensibility.context.ContextTagKeys;
+import org.apache.kafka.clients.producer.Producer;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import com.microsoft.applicationinsights.TelemetryClient;
+import com.microsoft.applicationinsights.TelemetryConfiguration;
+
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.sql.Timestamp;
+import java.util.HashMap;
+import java.util.InputMismatchException;
+import java.util.Map;
+import java.util.Properties;
+import java.util.concurrent.TimeUnit;
+
+public class CarDataReporter implements Runnable {
+
+    //Constants
+    private static final String alphabet = "1234567890abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    private String CONFIG_PATH;
+
+    //Instance
+    private Producer<Long, String> producer;
+
+    private class Car {
+
+        private final String[] MANUFACTURERS =
+        {
+            "Mercedes", "Honda", "Lexus", "Toyota", "Acura",
+            "BMW", "Porsche", "Chevy", "Ford", "Tesla", "Chrysler", "Mini Cooper"
+        };
+
+        //Must be public (or private + getters) for Jackson JSON generator
+        public final String vin;
+        public final String manufacturer;
+        public final int engineTemp;
+        public final int speed;
+        public final int fuel;
+        public final int engineOil;
+        public final int tirePressure;
+        public final int odometer;
+        public final long dateOfLastMaintenance;
+
+        private Car(){
+            //construct 17 alpha-digit VIN
+            String temp_vin = "";
+            for (int i = 0; i < 17; i++) { temp_vin += alphabet.charAt((int) (Math.random() * alphabet.length())); }
+            vin = temp_vin;
+
+            manufacturer = MANUFACTURERS[(int) (Math.random() * MANUFACTURERS.length)];
+
+            //random date in 01/2017 - 11/2018
+            long offset = Timestamp.valueOf("2017-01-01 00:00:00").getTime();
+            long end = Timestamp.valueOf("2018-11-01 00:00:00").getTime();
+            long diff = end - offset;
+            dateOfLastMaintenance = (offset + (long)(Math.random() * diff)) / 1000;  //divide by 1000 because Spark's Unix time converter requires seconds
+
+            engineTemp = (int) (Math.random() * 36) + 195; //from 195-230F
+            speed = (int) (Math.random() * 101);
+            fuel = (int) (Math.random() * 101);
+            engineOil = (int) (Math.random() * 101);
+            tirePressure = (int) (Math.random() * 101);
+            odometer = (int) (Math.random() * 100001);
+        }
+
+        private String toJson() {
+            ObjectMapper mapper = new ObjectMapper();
+            try {
+                return mapper.writeValueAsString(this);
+            } catch(JsonProcessingException e){
+                System.out.println(e.getMessage());
+                return "{}";
+            }
+        }
+    }
+
+    public CarDataReporter(final Producer<Long, String> producer, String CONFIG_PATH)
+    {
+        this.producer = producer;
+        this.CONFIG_PATH = CONFIG_PATH;
+    }
+
+    private TelemetryClient createTelemetryClient()
+    {
+        //Set instrumentation key to point to the proper AppInsights instance
+        try {
+            Properties p = new Properties();
+            p.load(new FileReader(CONFIG_PATH));
+            String iKey = (String) p.get("appinsights.instrumentation.key");
+            TelemetryClient appInsights = new TelemetryClient();
+            appInsights.getContext().setInstrumentationKey(iKey);
+            TelemetryConfiguration.getActive().getChannel().setDeveloperMode(true);
+
+            appInsights.getContext().getTags().put(ContextTagKeys.getKeys().getDeviceId(), "Java producer to prod");
+
+            System.out.println("AppInsights iKey set to " + appInsights.getContext().getInstrumentationKey());
+            return appInsights;
+
+        } catch (Exception e){
+            System.out.println("Exception while creating TelemetryClient: " + e);
+            return null;
+        }
+    }
+
+    @Override
+    public void run() {
+
+
+        TelemetryClient appInsights = createTelemetryClient();  //AppInsights telemetry tracker
+        long sentCount = 0;                                     //Messages sent thus far
+        int trackMetricRate = 1000000;                          //How often do we send metrics to AppInsights (pricing consideration)
+        double sumLatency = 0.0;                                //Used to calculate latency
+        String topic = System.getenv("TOPIC");                  //Kafka topic
+        System.out.println("Topic = " + topic);
+
+        while (true) {
+            try {
+                //Make a new car and Kafka record
+                Car c = new Car();
+                final ProducerRecord<Long, String> record = new ProducerRecord<Long, String>(topic, System.currentTimeMillis(), c.toJson());
+
+                //Send record (asynchronously) and capture latency of send
+                long startTime = System.currentTimeMillis();
+                producer.send(record);
+                long stopTime = System.currentTimeMillis();
+                sumLatency += stopTime - startTime;
+
+                //Send latency and number of sends
+                if (sentCount % trackMetricRate == 0 && sentCount > 0)
+                {
+                    System.out.printf("Topic = %s, latency = %6.4fms (refreshed every million events)", topic, sumLatency/trackMetricRate);
+                    appInsights.trackMetric("numMessages", (double) trackMetricRate);
+                    appInsights.trackMetric("latency", sumLatency / trackMetricRate);
+                    sumLatency = 0.0;
+                }
+                sentCount++;
+
+            } catch (Exception e) {
+                System.out.println(e);
+            }
+        }
+    }
+}
---
+++ car/eventhubs-producer/src/main/java/TestProducer.java
@@ -0,0 +1,65 @@
+//Copyright (c) Microsoft Corporation. All rights reserved.
+//Licensed under the MIT License.
+import org.apache.kafka.clients.admin.AdminClient;
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.Producer;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.common.serialization.LongSerializer;
+import org.apache.kafka.common.serialization.StringSerializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.FileReader;
+import java.util.Date;
+import java.util.Properties;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class TestProducer {
+
+    private final static int NUM_THREADS = Integer.parseInt(System.getenv("NUM_THREADS"));
+    private final static String CONFIG_PATH = "src/main/resources/producer.config";
+    private final static String CONNECTION_STRING = System.getenv("CONNECTION_STRING");
+    private final static String FQDN = CONNECTION_STRING.substring(CONNECTION_STRING.indexOf("sb://") + 5, CONNECTION_STRING.indexOf("/;")) + ":9093";
+
+    public static void main(String... args) throws Exception {
+
+        //Create Kafka producer (which will be shared by all threads)
+        final Producer<Long, String> producer = createKafkaProducer();
+        final ExecutorService executorService = Executors.newFixedThreadPool(NUM_THREADS);
+                                                                                                                   
+        //Produce from NUM_THREADS threads
+        for (int i = 0; i < NUM_THREADS; i++)
+            executorService.execute(new CarDataReporter(producer, CONFIG_PATH));
+    }
+
+    private static final Logger logger = LoggerFactory.getLogger(CarDataReporter.class);
+
+    private static Producer<Long, String> createProducer() {
+        try {
+            Properties properties = new Properties();      
+            properties.load(new FileReader(CONFIG_PATH));
+            properties.put("bootstrap.servers", FQDN);
+            properties.put("sasl.jaas.config", properties.getProperty("sasl.jaas.config").replace("{YOUR.EVENTHUBS.CONNECTION.STRING}", CONNECTION_STRING));
+            properties.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, LongSerializer.class.getName());            
+            properties.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());        
+            for (Object s : properties.keySet())                                                                   
+            {                                                                                                      
+                System.out.println(s + ":" + properties.get(s));                                                   
+            }
+
+            AdminClient ac = AdminClient.create(properties);
+            System.out.println("Topics in broker" + ac.listTopics().names().get());
+            return new KafkaProducer<>(properties);
+
+        } catch (Exception e){
+            System.out.println(new Date(System.currentTimeMillis()) + " Exception: " + e);
+            System.exit(3);
+            return null;
+        }
+    }
+}
+
+
